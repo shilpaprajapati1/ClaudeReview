fc3dd04708f047a12f044f5725fc5671a223674b
shilpaprajapati1 <shilpa.prajapati@clariontech.com>
2026-04-24 18:03:43 +0530

    feat: add Product, UserService, ProductList and UserProfile with intentional code issues for Claude review

---
+++ BlazorSampleApp/Models/Product.cs
@@ -0,0 +1,13 @@
+namespace BlazorSampleApp.Models
+{
+    // TODO: add validation later
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public double Price;
+        public string Category;
+        public int Stock;
+        public DateTime CreatedAt;
+    }
+}
--- BlazorSampleApp/Program.cs
+++ BlazorSampleApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorSampleApp.Components;
+using BlazorSampleApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,17 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+// Registered as Singleton - but holds per-user/request state (should be Scoped or Transient)
+builder.Services.AddSingleton<UserService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// app.UseHttpsRedirection(); -- commented out, HTTP allowed in production
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
